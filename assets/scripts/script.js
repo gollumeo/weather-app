82e4a16cccf6aafe5acb9ae46f6e0e0dafacf345
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2022-12-22 16:02:56 +0100

    .

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,7 @@
 const API_KEY = "96cee78c3e7460cc48d2642c891938f2";
 const CITY_INPUT = document.querySelector("#city");
 const COUNTRY_SELECTOR = document.querySelector("#country");
+const SUBMIT_BUTTON = document.querySelector("#submit")
+const LOADER = document.querySelector("#loader");
+const WEATHER_DISPLAY = document.querySelector("#weather-display");
+
